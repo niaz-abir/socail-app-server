f6a789292802b7c40edcc2b68c9c6a31d04877c8
niaz abir <niazabir55@gmail.com>
2022-12-29 15:00:29 +0600

    last commit

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const run = async () => {
   app.get("/posts", async (req, res) => {
     const result = await postsCollection.find({}).toArray();
     const sortedResult = result.sort((a, b) => b.love - a.love);
-    console.log(sortedResult);
+    // console.log(sortedResult);
     res.send(sortedResult);
   });
   app.post("/users/new", async (req, res) => {
@@ -43,16 +43,35 @@ const run = async () => {
 
   app.get("/users", async (req, res) => {
     const email = req.query.email;
-    console.log(req.query);
+    // console.log(req.query);
     const result = await usercollection.findOne({ email: email });
     res.send(result);
+  });
+
+  app.put("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: ObjectId(id) };
+    const user = req.body;
+    console.log(req.body);
+    const option = { upsert: true };
+    const updateuser = {
+      $set: {
+        name: user.name,
+        address: user.address,
+        email: user.email,
+      },
+    };
+    const result = await usercollection.updateOne(filter, updateuser, option);
+    res.send(result);
+
+    // console.log(updateuser);
   });
 
   app.get("/detail/:id", async (req, res) => {
     const postId = req.params.id;
     const response = await postsCollection.findOne({ _id: ObjectId(postId) });
 
-    console.log(response);
+    // console.log(response);
     res.json(response);
   });
 };
